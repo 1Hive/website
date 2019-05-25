b5638dd96fdc99d434c3a413c3b720a6c4942fea
lkngtn <luke@lukeduncan.net>
2019-05-25 18:11:32 -0400

    moved projects to bottom

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -19,7 +19,7 @@ class Index extends React.Component {
           <div className="content">
             <h1>Helping Open Source Communities Thrive</h1>
             <h2>
-              1Hive is a decentralized autonomous organization working to grow the open source commons and improve open source sustainability.
+              1Hive is a decentralized autonomous organization working to grow an open source commons and improve open source sustainability.
             </h2>
             <div className="row">
               <a
@@ -40,7 +40,7 @@ class Index extends React.Component {
             <div className="shadow" />
           </div>
         </div>
-			)	
+			)
 		}
 
 
@@ -50,14 +50,14 @@ class Index extends React.Component {
         <div className="content row">
           <div className="col center">
             <img
-              src="/img/051-sprout-1.png" width="250"
+              src="/img/flowers/lotus.png" width="200"
             />
           </div>
           <div className="col">
             <h4>Tools</h4>
-            <h3>Accountable Crowdfunding</h3>
+            <h3>Digital Cooperatives</h3>
             <p>
-              We are contributing to the research and development of <strong>Apiary</strong>, a crowdfunding platform for decentralized autonomous organizations. Open Source projects can use Apiary to grant patrons exclusive rights and privelleges within their community.
+              We are building applications which help communities securely and transparently operate digital cooperatives. These organizational tools can help open source projects attract and fairly reward contributors.
             </p>
             <a className="learnmore" href="/docs/projects/apiary">
               Learn more
@@ -73,7 +73,7 @@ class Index extends React.Component {
         <div className="content row">
           <div className="col">
             <h4>Licensing</h4>
-            <h3>Hive Commons License</h3>
+            <h3>Growing the Commons</h3>
             <p>
               We believe that finding sustainable funding sources for open source projects is critical to growing the open source commons. To that end we are advocating for a novel open source license which combines the best aspects of <strong>copyleft</strong> and <strong>permissive</strong> style open source licensing.
             </p>
@@ -83,7 +83,7 @@ class Index extends React.Component {
           </div>
           <div className="col center">
             <img
-              src="/img/051-flower.png" width="250"
+              src="/img/flowers/sunflower.png" width="200"
             />
           </div>
         </div>
@@ -96,7 +96,7 @@ class Index extends React.Component {
         <div className="content row">
           <div className="col center">
             <img
-              src="/img/051-gloves.png" width="250"
+              src="/img/051-gloves.png" width="200"
             />
           </div>
           <div className="col">
@@ -116,39 +116,27 @@ class Index extends React.Component {
 			)
 		}
 
-		// Fourth Content Component
-		function Content4() {
-			return (
-        <div className="content row">
-          <div className="col center">
-            <img
-              src="/img/051-gloves.png" width="250"
-            />
-          </div>
-          <div className="col">
-            <h4>Get Some Honey</h4>
-            <h3>How to get HONEY</h3>
-            <p>
-              We are an open and transparent organization. This enables you to see and understand how we
-              operate, and of course join the community and help us improve. Many of our projects, both technical and non-technical, have <strong>open bounties</strong>.
-            </p>
-            <a
-              className="learnmore"
-              href="/docs/contribute/welcome">
-              Learn more
-            </a>
-          </div>
-        </div>
-			)
-		}
-
 		// Projects Component
 		function Projects() {
 			return (
-        <div className="wrapper landing-cta">
-          <a href="/docs/projects/projects-welcome" className="landing-btn primary">
-            More projects
-          </a>
+        <div className="wrapper col projects-wrapper">
+          <div className="projects" align="center">
+            <h4>Projects</h4>
+            <ul>
+              <li>
+                <span><a href="/docs/projects/redemptions.html"><img src="/img/flowers/poppy.png" /></a></span>
+                <span>Redemptions</span>
+                </li>
+              <li>
+                <span><a href="/docs/projects/commons-license.html"><img src="/img/flowers/chrysanthemum.png" /></a></span>
+                <span>Commons License</span>
+                </li>
+              <li>
+                <span><a href="/docs/projects/apiary.html"><img src="/img/flowers/wallflower.png" /></a></span>
+                <span>Apiary</span>
+                </li>
+            </ul>
+          </div>
         </div>
 			)
 		}
