0ab751cf58f26d133bb01b0be744dced42f4fc98
burrrata <42473928+burrrata@users.noreply.github.com>
2019-08-05 16:40:15 -0400

    works on my machine! lol

--- website/scripts/sync-dandelion-apps.js
+++ website/scripts/sync-dandelion-apps.js
@@ -1,10 +1,10 @@
-const { syncPages } = require('./sync-util')
+const { syncPages } = require("./sync-util");
 
-const GIT_REF = 'master'
-const REPO = '1hive/redemptions-app'
+const GIT_REF = "master";
+const REPO = "1hive/redemptions-app";
 
 /*
-NOTE! 
+NOTE!
 - the contentLocation fields are CASE SENSITIVE
 - there needs to be placeholder content in the sync directory before you can run the sync scripts (because the way they're written they overwrite old versions of the docs in that location)
 - you need to tell package.json to clear out the old sync directory and run the sync scripts to add new content
@@ -12,17 +12,14 @@ NOTE!
 */
 
 const pages = [
+  /* Redemptions Overview */
+  {
+    destination: "docs/sync/redemptions-overview.md",
+    title: "1Hive Redemptions App",
+    contentLocation: "docs/overview.md"
+  }
+];
 
-	/* Redemptions Overview */
-	{
-		destination: 'docs/sync/dandelion/redemptions-overview.md',
-		id: 'redemptions-overview',
-		sidebarLabel: 'Redemptions',
-		title: '1Hive Redemptions App',
-		contentLocation: 'docs/overview.md',
-	}
-]
+const locationReferenceMap = {};
 
-const locationReferenceMap = {}
-
-syncPages(pages, locationReferenceMap, GIT_REF, REPO)
+syncPages(pages, locationReferenceMap, GIT_REF, REPO);
